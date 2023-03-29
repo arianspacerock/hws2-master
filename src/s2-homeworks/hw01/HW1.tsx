ee880bfaacf4ed0d869f3369fcdb4cf9d33b7ecb
arianspacerock <siropemptyspace@gmail.com>
2023-03-29 12:20:05 +0300

    ref: change text

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -34,7 +34,7 @@ export const message0: MessageType = {
         name: 'Artem',  // можно менять
     },
     message: {
-        text: 'some text', // можно менять
+        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
 }
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -28,7 +28,7 @@ const Message = (props: MessagePropsType) => {
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
-                        Hi, how are you?
+
                         {/**/}
                     </pre>
                 </div>
