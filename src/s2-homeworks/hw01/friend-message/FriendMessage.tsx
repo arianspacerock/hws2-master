0ec5db8c2a88b77c9ca08d9cb3d84728abdf363a
arianspacerock <siropemptyspace@gmail.com>
2023-03-29 11:57:55 +0300

    ref: add types for HW1

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import avatar from './../avatar.png'
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+type FriendMessagePropsType = {
+    message: {
+        id: number
+    }
+}
+
+
+const FriendMessage = (props: FriendMessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
@@ -12,7 +20,7 @@ const FriendMessage = (props: any) => {
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
                     // создаёт студент
-
+                    src={avatar} alt={'pic'}
                     //
                 />
                 <div className={s.friendText}>
@@ -21,7 +29,7 @@ const FriendMessage = (props: any) => {
                         className={s.friendName}
                     >
                         {/*создаёт студент*/}
-
+                        Max
                         {/**/}
                     </div>
                     <pre
@@ -29,7 +37,7 @@ const FriendMessage = (props: any) => {
                         className={s.friendMessageText}
                     >
                         {/*создаёт студент*/}
-
+                        I`m fine, thanks, are you?
                         {/**/}
                     </pre>
                 </div>
@@ -39,7 +47,7 @@ const FriendMessage = (props: any) => {
                 className={s.friendTime}
             >
                 {/*создаёт студент*/}
-
+                9:05
                 {/**/}
             </div>
         </div>
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import s from './Message.module.css'
+import avatar from './../avatar.png'
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: {
+        id: number
+    }
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
@@ -12,25 +17,25 @@ const Message = (props: MessagePropsType) => {
                 <img
                     id={'hw1-avatar-' + props.message.id}
                     // создаёт студент
-
+                    src={avatar} alt={'pic'}
                     //
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
-
+                        Artem
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
-
+                        Hi, how are you?
                         {/**/}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
-
+                9:00
                 {/**/}
             </div>
         </div>
