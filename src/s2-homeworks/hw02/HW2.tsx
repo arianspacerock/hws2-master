2898dd525d26a3355ec09258ba9cf2286b7e68a6
arianspacerock <siropemptyspace@gmail.com>
2023-04-11 17:53:58 +0300

    ref: add type for HW2 & logic

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -36,23 +36,24 @@ const defaultAffairs: AffairType[] = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => {
+    if (filter === 'all') return affairs
+    else if (filter === 'high') return affairs.filter(a => a.priority === 'high')
+    else if (filter === 'middle') return affairs.filter(a => a.priority === 'middle')
+    else if (filter === 'low') return affairs.filter(a => a.priority === 'low')
+    return affairs
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-
-    return affairs // need to fix
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
+    return affairs.filter(a => a._id !== _id)
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    const deleteAffairCallback = (_id: number) => { // need to fix any
+        setAffairs(affairs.filter(a => a._id !== _id))
     }
 
     return (
