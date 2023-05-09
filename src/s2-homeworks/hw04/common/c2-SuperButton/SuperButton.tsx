0fc811265e6269279eb94abc0c265c21defdfca3
arianspacerock <siropemptyspace@gmail.com>
2023-05-09 11:15:09 +0300

    ref: add styles for HW4 & logic

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -19,12 +19,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
 ) => {
     const finalClassName = s.button
          + (disabled
-                 ? ' ' + s.disabled
+                 ? s.disabled
                  : xType === 'red'
-                     ? ' ' + s.red
+                     ? s.red
                      : xType === 'secondary'
-                         ? ' ' + s.secondary
-                         : '')
+                         ? s.secondary
+                         : s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
