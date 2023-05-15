8d6c9e5096ed8a0c111cb981ad4a242f8f45e40f
arianspacerock <siropemptyspace@gmail.com>
2023-05-15 18:42:34 +0300

    ref: add logic for HW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -50,7 +50,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       name={name}
-                      value={o.value}
+                      value={o.id}
                       checked={o.id === value}
                       // name, checked, value делают студенты
 
