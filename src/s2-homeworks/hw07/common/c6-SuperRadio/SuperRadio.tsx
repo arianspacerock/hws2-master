85223519dc1dc1d86c033315daab123f87b8a42b
arianspacerock <siropemptyspace@gmail.com>
2023-05-15 18:24:09 +0300

    ref: add logic for HW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,6 +35,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChangeOption && onChangeOption(e.currentTarget.value)
         // делают студенты
     }
 
@@ -48,6 +49,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      name={name}
+                      value={o.id}
+                      checked={value === o.id}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
