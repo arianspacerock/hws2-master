9247f6c9fe066d259441f9e6bab2b11f84881461
arianspacerock <siropemptyspace@gmail.com>
2023-06-28 22:07:25 +0300

    ref: start add logic for HW10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -5,13 +5,14 @@ const initState = {
 export const loadingReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
 }
 
-type LoadingActionType = {
+export type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
