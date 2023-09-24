41b61bf2a495d37d4afb5532064b6bd24f817b94
arianspacerock <siropemptyspace@gmail.com>
2023-09-24 15:03:02 +0300

    ref: start add logic for HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,13 +15,13 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: number | number[]) => {
+    const change = (event: ChangeEvent<HTMLInputElement>, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         if (Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
         } else {
-            setValue1(value)
+            setValue1(Number(event.target.value))
         }
     }
 
@@ -37,9 +37,7 @@ function HW11() {
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
                             value={value1}
-
-
-
+                            onChange={change}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -48,8 +46,7 @@ function HW11() {
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
                             value={[value1, value2]}
-
-
+                            onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
